3885c9f4af3b11886d1625d66631448bf163a081
jayam-joshi <82452505+jayam-joshi@users.noreply.github.com>
2021-10-24 00:18:44 +0530

    Update index.py

--- index.py
+++ index.py
@@ -8,37 +8,34 @@ from ml import prediction
 
 bg = None
 
-# initialize weight for running average
 aWeight = 0.5
 
-# get the reference to the webcam
 camera = cv2.VideoCapture(0)
 
-# region of interest (ROI) coordinates
 top, right, bottom, left = 10, 350, 225, 590
 
-# initialize num of frames
+
 num_frames = 0
 
 i = 0 
-# keep looping, until interrupted
+
 while(True):
         # get the current frame
         (grabbed, frame) = camera.read()
 
-        # resize the frame
+        
         frame = imutils.resize(frame, width=700)
 
-        # flip the frame so that it is not the mirror view
+        
         frame = cv2.flip(frame, 1)
 
-        # clone the frame
+        
         clone = frame.copy()
 
-        # get the height and width of the frame
+        
         (height, width) = frame.shape[:2]
 
-        # get the ROI
+        
         roi = frame[top:bottom, right:left]
 
         # convert the roi to grayscale and blur it
@@ -50,13 +47,13 @@ while(True):
         if num_frames < 30:
             run_avg(gray, aWeight)
         else:
-            # segment the hand region
+            
             hand = segment(gray)
 
-            # check whether hand region is segmented
+            
             if hand is not None:
-                # if yes, unpack the thresholded image and
-                # segmented region
+                
+                
                 (thresholded, segmented) = hand
 
                 # draw the segmented region and display the frame
@@ -83,14 +80,14 @@ while(True):
         # display the frame with segmented hand
         cv2.imshow("Video Feed", clone)
 
-        # observe the keypress by the user
+        
         keypress = cv2.waitKey(1) & 0xFF
 
-        # if the user pressed "q", then stop looping
+        
         if keypress == ord("q"):
             break
 
-# free up memory
+
 camera.release()
 cv2.destroyAllWindows()
 
